81656d0065096fb534f350a6e977c89fb9adf12d
Alexander Lee <alex8usa8@gmail.com>
2017-09-29 13:07:17 -0700

    On the press up event of the spacebar, the car starts. #2

--- main.js
+++ main.js
@@ -12,17 +12,54 @@ function renderBoundary() {
   return $div
 }
 
+const carPrototype = {
+  turn(newDirection) {
+    this.direction = newDirection
+  },
+  accelerate(newSpeed) {
+    this.speed = newSpeed
+  },
+  move() {
+    if (this.direction === 'north') {
+      this.location[1] = this.location[1] - this.speed
+    }
+    else if (this.direction === 'south') {
+      this.location[1] = this.location[1] + this.speed
+    }
+    else if (this.direction === 'east') {
+      this.location[0] = this.location[0] - this.speed
+    }
+    else if (this.direction === 'west') {
+      this.location[0] = this.location[0] + this.speed
+    }
+  }
+}
+
+function startCar() {
+  $car.move()
+  const currentLocation = $car.location
+  Object.assign($car.style, {
+    left: currentLocation[0] + 'px',
+    top: currentLocation[1] + 'px'
+  })
+}
+
 const $car = renderCar()
+$car['direction'] = 'south'
+$car['speed'] = 10
+$car['location'] = [0, 0]
+Object.assign($car, carPrototype)
 Object.assign($car.style, {
   left: '0px',
   top: '0px'
 })
+
 const $boundary = renderBoundary()
 $boundary.appendChild($car)
 document.body.appendChild($boundary)
 
 document.body.onkeyup = (event) => {
   if (event.code === 'Space') {
-    console.log('test')
+    setInterval(startCar, 1000)
   }
 }
